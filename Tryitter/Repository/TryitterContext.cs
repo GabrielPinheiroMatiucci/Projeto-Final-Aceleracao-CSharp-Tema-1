a45b0ff67d916a9ed7bf9293882e528e4608b906
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-02-07 16:55:56 -0300

    feat: add initial config or context and repository of Students.

---
+++ Tryitter/Repository/TryitterContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Tryitter.Models;
+
+namespace Tryitter.Repository;
+
+public class TryitterContext : DbContext
+{
+  public TryitterContext(DbContextOptions<TryitterContext> options) : base(options) {}
+
+  public DbSet<Student> Students { get; set; }
+}
