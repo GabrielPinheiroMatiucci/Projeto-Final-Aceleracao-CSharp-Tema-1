6e0d205da017a9f78db33458d048c801d096b327
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-02-13 19:28:20 -0300

    feat: add UpdateStudent and add return to CreateStudent.

--- Tryitter/Repository/TryitterRepository.cs
+++ Tryitter/Repository/TryitterRepository.cs
@@ -16,9 +16,22 @@ public class TryitterRepository
     return _context.Students.ToList();
   }
 
-  public void CreateStudent(Student student)
+  public int CreateStudent(Student student)
   {
     _context.Students.Add(student);
     _context.SaveChanges();
+
+    return student.Id;
+  }
+
+  public bool UpdateStudent(int id, Student student)
+  {
+    if (!_context.Students.Any(std => std.Id == id) || id != student.Id)
+      return false;
+
+    _context.Students.Update(student);
+    _context.SaveChanges();
+
+    return true;
   }
 }
