ad88f5e9fe3dd7bc22f881d48e79d806327922d2
Marcela Silva <marcela.ops@hotmail.com>
2023-02-15 10:28:43 -0300

    9 - trying to solve conflicts at `TestController.cs`

--- src/Tryitter.Test/TestStudentsController.cs
+++ src/Tryitter.Test/TestStudentsController.cs
@@ -9,7 +9,7 @@ using Tryitter.Repository;
 namespace Tryitter.Test;
 
 public class TestStudentsController : IClassFixture<WebApplicationFactory<Program>>
-{   
+{
   private readonly WebApplicationFactory<Program> _factory;
 
   public TestStudentsController(WebApplicationFactory<Program> factory)
@@ -21,44 +21,36 @@ public class TestStudentsController : IClassFixture<WebApplicationFactory<Progra
   public async Task TestGetStudents()
   {
     HttpClient _client = _factory.CreateClient();
-
     List<Student> mockStudents = new List<Student>()
     {
       new Student("string", "email", "string", "password") { Id = 1 },
       new Student("string", "email", "string", "password") { Id = 2 },
     };
-
     var mockGet = new Mock<TryitterRepository>();
     mockGet
       .Setup(m => m.GetStudents())
       .Returns(mockStudents);
-
     HttpResponseMessage response = await _client.GetAsync("/students");
     List<Student> content = await response.Content.ReadFromJsonAsync<List<Student>>();
     
     response.StatusCode.Should().Be(HttpStatusCode.OK);
     content.Should().NotBeNullOrEmpty();
   }
-
   [Fact]
   public async Task TestGetStudent()
   {
     HttpClient _client = _factory.CreateClient();
-
     Student mockStudent = new("string", "email", "string", "password") { Id = 1 };
-
     var mockGet = new Mock<TryitterRepository>();
     mockGet
       .Setup(m => m.GetStudent(It.IsAny<int>()))
       .Returns(mockStudent);
-
     HttpResponseMessage response = await _client.GetAsync("/students/1");
     Student content = await response.Content.ReadFromJsonAsync<Student>();
     
     response.StatusCode.Should().Be(HttpStatusCode.OK);
     content.Should().NotBeNull();
   }
-
   [Theory]
   [InlineData("email@email.com", "123")]
   public async Task TestCreateSuccess(string email, string password)
@@ -66,32 +58,25 @@ public class TestStudentsController : IClassFixture<WebApplicationFactory<Progra
     HttpClient _client = _factory.CreateClient();
     Student mockStudent = new("string", email, "string", password);
     Credentials credentials = new(email, password);
-
     var mockGet = new Mock<TryitterRepository>();
     mockGet
       .Setup(m => m.GetStudent(It.IsAny<int>()))
       .Returns(mockStudent);
-
     var mockUpdate = new Mock<TryitterRepository>();
     mockUpdate
       .Setup(m => m.CreateStudent(It.IsAny<Student>()))
       .Returns(1);
-
     HttpResponseMessage loginResponse = await _client
       .PostAsJsonAsync("/login", mockStudent);
     string token = await loginResponse.Content.ReadAsStringAsync();
-
     token.Should().NotBeNull();
-
     _client
       .DefaultRequestHeaders
       .Authorization = new AuthenticationHeaderValue("Bearer", token);
     HttpResponseMessage response = await _client.PostAsJsonAsync("/students", mockStudent);
-
     response.StatusCode.Should().Be(HttpStatusCode.Created);
     mockUpdate.Verify(m => m.CreateStudent(It.IsAny<Student>()), Times.Once);
   }
-
   [Theory]
   [InlineData("email@email.com", "123")]
   public async Task TestPutSuccess(string email, string password)
@@ -99,47 +84,36 @@ public class TestStudentsController : IClassFixture<WebApplicationFactory<Progra
     HttpClient _client = _factory.CreateClient();
     Student mockStudent = new("string", email, "string", password) { Id = 99 };
     Credentials credentials = new(email, password);
-
     var mockGet = new Mock<TryitterRepository>();
     mockGet
       .Setup(m => m.GetStudent(It.IsAny<int>()))
       .Returns(mockStudent);
-
     var mockUpdate = new Mock<TryitterRepository>();
     mockUpdate
       .Setup(m => m.UpdateStudent(It.IsAny<int>(), It.IsAny<Student>()))
       .Returns(true);
-
     HttpResponseMessage loginResponse = await _client
       .PostAsJsonAsync("/login", mockStudent);
     string token = await loginResponse.Content.ReadAsStringAsync();
-
     token.Should().NotBeNull();
-
     _client
       .DefaultRequestHeaders
       .Authorization = new AuthenticationHeaderValue("Bearer", token);
     HttpResponseMessage response = await _client.PutAsJsonAsync("/students/99", mockStudent);
-
     response.StatusCode.Should().Be(HttpStatusCode.NoContent);
   }
-
   [Theory]
   [InlineData("123456789")]
   public async Task TestPutFail(string invalidToken)
   {
     HttpClient _client = _factory.CreateClient();
     Student mockStudent = new("string", "email", "string", "password") { Id = 1 };
-
     _client
       .DefaultRequestHeaders
       .Authorization = new AuthenticationHeaderValue("Bearer", invalidToken);
-
     HttpResponseMessage response = await _client.PutAsJsonAsync("/students/1", mockStudent);
-
     response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
   }
-
   [Theory]
   [InlineData("email@email.com", "123")]
   public async Task TestDeleteSuccess(string email, string password)
@@ -147,28 +121,22 @@ public class TestStudentsController : IClassFixture<WebApplicationFactory<Progra
     HttpClient _client = _factory.CreateClient();
     Student mockStudent = new("string", email, "string", password) { Id = 1 };
     Credentials credentials = new(email, password);
-
     var mockGet = new Mock<TryitterRepository>();
     mockGet
       .Setup(m => m.GetStudent(It.IsAny<int>()))
       .Returns(mockStudent);
-
     var mockUpdate = new Mock<TryitterRepository>();
     mockUpdate
       .Setup(m => m.DeleteStudent(It.IsAny<int>()))
       .Returns(true);
-
     HttpResponseMessage loginResponse = await _client
       .PostAsJsonAsync("/login", mockStudent);
     string token = await loginResponse.Content.ReadAsStringAsync();
-
     token.Should().NotBeNull();
-
     _client
       .DefaultRequestHeaders
       .Authorization = new AuthenticationHeaderValue("Bearer", token);
     HttpResponseMessage response = await _client.DeleteAsync("/students/1");
-
     response.StatusCode.Should().Be(HttpStatusCode.NoContent);
   } */
 }
