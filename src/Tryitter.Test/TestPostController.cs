5e8dd5da4b2bdd86d155a3feac0762c389a4d28b
Marcela Silva <marcela.ops@hotmail.com>
2023-02-15 14:36:02 -0300

    starting `TestPostController.cs`

--- src/Tryitter.Test/TestPostController.cs
+++ src/Tryitter.Test/TestPostController.cs
@@ -1,1 +1,25 @@
-// Initial commit
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Net;
+using System.Net.Http.Json;
+using Moq;
+using Tryitter.Models;
+using Tryitter.Repository;
+using Tryitter.Token;
+using Tryitter.Controllers;
+// using Tryitter.Interfaces;
+
+namespace Tryitter.Test;
+
+public class TestPostController : IClassFixture<WebApplicationFactory<Program>>
+{
+  private readonly WebApplicationFactory<Program> _factory;
+
+  public TestPostController(WebApplicationFactory<Program> factory)
+  {
+    _factory = factory;
+  }
+
+
+}
