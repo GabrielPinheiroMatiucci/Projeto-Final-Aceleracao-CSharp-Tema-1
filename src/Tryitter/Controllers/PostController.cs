7c99462e998d0d3d63524083f8621a1fe5123405
Marcela Silva <marcela.ops@hotmail.com>
2023-02-14 21:23:46 -0300

    feat: continuing `PostController.cs`

--- src/Tryitter/Controllers/PostController.cs
+++ src/Tryitter/Controllers/PostController.cs
@@ -14,9 +14,42 @@ public class PostController : ControllerBase
     _repository = repository;
   }
 
-  [HttpGet]
-  public IActionResult GetPosts(int id)
+  [HttpGet("post")] /* talvez faria o caminho como sendo: student/id/post */
+  public IActionResult GetPosts(int id) /* ainda na dúvida se deeveria colocar o id do student */
   {
     return Ok(_repository.GetPosts(id));
   }
+
+  [HttpGet]
+  public IActionResult GetLastPost(int id)
+  {
+    return Ok(_repository.GetLastPost(id));
+  }
+
+  // [HttpPost]
+  // public IActionResult CreatePost(int id, Post post)
+  // {
+  //   return Ok(_repository.CreatePost(id, post));
+  // }
+  //   [HttpPost]
+  // public IActionResult 
+  // {
+  // return Ok(_repository.);
+
+  // }
+
+  // [HttpGet]
+  // public IActionResult
+  // {
+  // return Ok(_repository.);
+  // 
+  // }
+
+  // [HttpGet]
+  // public IActionResult
+  // {
+  // return Ok(_repository.);
+  // 
+  // }
+
 }
